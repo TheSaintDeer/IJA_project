4ea73d28bd8d38e14a5d865edf6c5785dfa5ca04
ivan <golikov244@gmail.com>
2022-04-05 21:31:40 +0200

    Start working

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Database");
         stage.setScene(scene);
         stage.show();
     }
