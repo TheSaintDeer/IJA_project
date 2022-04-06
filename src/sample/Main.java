9adc9cb3b76e99911d10b103c06a5b22185a3d2f
ivan <golikov244@gmail.com>
2022-04-06 21:20:09 +0200

    Add UML

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,22 +2,36 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.uml.ClassDiagram;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    public static Scene scene;
+    public static ClassDiagram classDiagram;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/sample.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1024, 720);
-        stage.setTitle("Database");
+        sceneSet(stage, "fxml/sample.");
+    }
+
+    private void sceneSet(Stage stage, String fxml) throws IOException {
+        scene = new Scene(loadFXML(fxml));
         stage.setScene(scene);
+        stage.setTitle("Database");
         stage.show();
     }
 
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + "fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
+        classDiagram = new ClassDiagram("Diagram");
         launch();
     }
 }
