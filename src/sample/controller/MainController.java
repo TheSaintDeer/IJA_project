4d85b1935954a4f7f50f9865a9e0b6b01dac73c8
ivan <golikov244@gmail.com>
2022-04-12 03:00:50 +0200

    fix

--- src/sample/controller/MainController.java
+++ src/sample/controller/MainController.java
@@ -1,11 +1,17 @@
 package sample.controller;
 
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.shape.Line;
 import sample.Main;
 import sample.controller.MainController;
 import sample.uml.ClassDiagram;
@@ -13,6 +19,11 @@ import sample.uml.ClassDiagram;
 import java.io.IOException;
 
 public class MainController extends Main {
+
+    private int countOfRow = 0;
+
+    @FXML
+    private Button acceptRelat;
 
     @FXML
     private Button closeWindow;
@@ -31,6 +42,15 @@ public class MainController extends Main {
 
     @FXML
     private GridPane grPane;
+
+    @FXML
+    private Label labelFrom;
+
+    @FXML
+    private Label labelTo;
+
+    @FXML
+    private AnchorPane mainPane;
 
     @FXML
     private TextField toClassX;
@@ -53,9 +73,31 @@ public class MainController extends Main {
         closeWindow.setOnAction(event -> {
             closeWindow.getScene().getWindow().hide();
         });
+        createRelat.setOnAction(event -> {
+            visibleObject(true);
+        });
+        acceptRelat.setOnAction(event -> {
+            visibleObject(false);
+            Node fromC = getNodeByRowColumnIndex(Integer.parseInt(fromClassY.getText()), Integer.parseInt(fromClassX.getText()));
+            Node toC = getNodeByRowColumnIndex(Integer.parseInt(toClassY.getText()), Integer.parseInt(toClassX.getText()));
+            drawNewLine(fromC, toC);
+
+
+//            Line line = new Line(0, 0, 300, 0);
+//            line.setStartX(fromC.getLayoutX() + 5);
+//            line.setStartY(fromC.getLayoutY());
+//            line.setEndX(fromC.getLayoutX() + grPane.getColumnConstraints().get(Integer.parseInt(fromClassX.getText())).getPrefWidth() + 5);
+//            line.setEndY(fromC.getLayoutY() + grPane.getRowConstraints().get(Integer.parseInt(fromClassX.getText())).getPrefHeight());
+//            grPane.add(line, 0, 0);
+//            mainPane.getChildren().add(line);
+
+
+            clearField();
+        });
     }
 
     void createNewClass() {
+        checkRow();
         grPane.add(getMyParent(), (countOfClass % 4), (countOfClass / 4));
         countOfClass++;
     }
@@ -75,5 +117,59 @@ public class MainController extends Main {
 
         return loader.getRoot();
     }
+
+    private void visibleObject (boolean switcher) {
+        labelFrom.setVisible(switcher);
+        labelTo.setVisible(switcher);
+        fromClassX.setVisible(switcher);
+        fromClassY.setVisible(switcher);
+        toClassX.setVisible(switcher);
+        toClassY.setVisible(switcher);
+        acceptRelat.setVisible(switcher);
+    }
+
+    public Node getNodeByRowColumnIndex (final int row, final int column) {
+        Node result = null;
+        ObservableList<Node> childrens = grPane.getChildren();
+
+        for (Node node : childrens) {
+            if(GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column) {
+                result = node;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public void clearField () {
+        fromClassX.setText("");
+        fromClassY.setText("");
+        toClassX.setText("");
+        toClassY.setText("");
+    }
+
+    private void drawNewLine (Node from, Node to) {
+        Line line = new Line();
+        line.setStartX(from.getLayoutX() + (grPane.getColumnConstraints().get(Integer.parseInt(fromClassX.getText())).getPrefWidth() / 2) + 5);
+        line.setStartY(from.getLayoutY() + grPane.getRowConstraints().get(Integer.parseInt(fromClassY.getText())).getPrefHeight());
+        line.setEndX(to.getLayoutX() + (grPane.getColumnConstraints().get(Integer.parseInt(toClassX.getText())).getPrefWidth() / 2) + 5);
+        line.setEndY(to.getLayoutY());
+
+
+        mainPane.getChildren().add(line);
+//        grPane.add(line, 0, 0);
+    }
+
+    public void checkRow() {
+        if (countOfRow < (countOfClass / 4)) {
+            countOfRow++;
+            RowConstraints row = new RowConstraints();
+            row.setMinHeight(240);
+            row.setFillHeight(false);
+            grPane.getRowConstraints().addAll(row);
+        }
+    }
+
 
 }
