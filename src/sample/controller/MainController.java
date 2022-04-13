26778098965a1ff2d7a0efbb787d8e1024db1360
ivan <golikov244@gmail.com>
2022-04-13 21:51:00 +0200

    FIX

--- src/sample/controller/MainController.java
+++ src/sample/controller/MainController.java
@@ -148,8 +148,6 @@ public class MainController extends Main {
                 clearField();
             } else {
                 visibleObject(false);
-                typeRelat.setText("Type relat");
-                clearField();
 
                 String type = "";
                 if (typeRelat.getText() == "ASSOCIACE") {
@@ -161,6 +159,10 @@ public class MainController extends Main {
                 } else if (typeRelat.getText() == "GENERALIZACE") {
                     type = "<|--";
                 }
+
+                typeRelat.setText("Type relat");
+                clearField();
+
                 if (diagram.findClass(fromClass.getText()) == null  || diagram.findClass(toClass.getText()) == null) {
                     terminalErrors.setText("Don't exist one of classes");
                 } else {
