f21824f83bcb875c1eb3d36a08108895c7a87e1e
kovalmaksym <makskv080@gmail.com>
2022-05-12 05:22:52 +0200

    last changes

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#wget https://download2.gluonhq.com/openjfx/11/openjfx-11_linux-x64_bin-sdk.zip
-#unzip openjfx-11_linux-x64_bin-sdk.zip
+wget https://download2.gluonhq.com/openjfx/11/openjfx-11_linux-x64_bin-sdk.zip
+unzip openjfx-11_linux-x64_bin-sdk.zip
 wget -O com.google.gson.jar  https://search.maven.org/remotecontent?filepath=com/google/code/gson/gson/2.9.0/gson-2.9.0.jar
